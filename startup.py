aebcb5ff1dab4088fdddc35fcbf690f608d7677f
RedstoneAlmeida <mineps3marcos@gmail.com>
2017-05-23 17:10:42 -0300

    3.4 is stable

--- startup.py
+++ startup.py
@@ -3,9 +3,9 @@ os.chdir(os.path.realpath(sys.argv[0]).replace(str(os.sep) + "startup.py", ""))
 from trinket.trinket import Trinket
 from trinket.utils.config import Config
 
-if sys.version_info < (3,5):
+if sys.version_info < (3,4):
     print("It looks like you're running an unsupported version of Python!")
-    print("Please update python to 3.5+")
+    print("Please update python to 3.4+")
     os._exit(0)
 else:
     cfg = Config.handle()
